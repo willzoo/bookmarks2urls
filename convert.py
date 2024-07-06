a71362257233ff808e382f0e57a7d639a9e82630
willzoo <zhujwill@gmail.com>
2024-07-06 16:12:20 -0400

    Initial file setup

---
+++ convert.py
@@ -0,0 +1,7 @@
+
+if __name__ == '__main__':
+    folder_path = 'Bookmarks'
+    print('Enter name of bookmark file: ')
+    file_name = input()
+    file_path = f'{folder_path}/{file_name}'
+    print(file_path)
